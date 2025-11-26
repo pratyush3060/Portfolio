522889b63ab872d120167d06bb086a08b73384e9
pratyush3060 <tpratyush0@gmail.com>
2025-11-26 20:24:28 +0530

    Fix duplicate Portfolio import and update .gitignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Portfolio from './components/Portfolio';
-import Portfolio from './components/Portfolio';
 import Admin from './components/Admin';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
